e5c0b873eba431fda6a5423a336c0e1f0b3eb46c
Michael Becco <e4e6d4d5@gmail.com>
2015-04-05 21:12:42 -0700

    Initial Scaffold for Contact Page
    Add first test of page

--- test/feature/visitor_email_test.rb
+++ test/feature/visitor_email_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
-feature "VisitorEmail" do
-  scenario "the test is sound" do
+feature "Visitor Can Contact Me Through Email" do
+  scenario "Fills in form and sends" do
     visit root_path
-    page.must_have_content "Hello World"
+    page.must_have_content "Contact"
     page.wont_have_content "Goobye All!"
   end
 end
